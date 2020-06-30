0a1394021665c8bbb1ad24dda0788e911d4ee627
youxingxiang <1365831278@qq.com>
2020-06-30 15:28:08 +0800

    构建

--- webhook.sh
+++ webhook.sh
@@ -1,15 +1,13 @@
 #! /bin/bash
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/software/node-v10.15.3-linux-x64/bin:/usr/local/software/go/bin
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/software/node-v10.15.3-linux-x64/bin:/usr/local/software/go/bin:/usr/local/go/bin/go
 
 cd /var/www/html/wxyadpi
 
 echo "更新代码"
 
-git pull -v --all
+git pull -v --all && /usr/local/go/bin/go build -o wxyapi /var/www/html/wxyadpi && nohup /var/www/html/wxyadpi/wxyapi >> /var/log/wxyapi 2>&1 &
 
-go build -o wxyapi .
 
-./wxyapi
 
 
